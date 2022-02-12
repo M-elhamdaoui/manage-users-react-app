d69bbdc1716048af8a2507d360a4ec29ecbe761c
melhamdaoui811 <melhamdaoui811@gmail.com>
2022-02-12 15:43:27 +0100

    add some style to User component

--- src/Component/User/User.js
+++ src/Component/User/User.js
@@ -4,12 +4,29 @@ import styled from 'styled-components'
 export default function User(props) {
   return (
     <UserCont>
-        <p>{props.name} (age : {props.age}yo)</p>
+        <p>Mohammed (age : 22yo)</p>
     </UserCont>
   )
 }
 
-const UserCont=styled.div`
-    width: auto;
-    background-color: #222;
-`
+const UserCont = styled.div`
+  color: #0d0d0d;
+  background-color: #bfbfbf;
+  height: 3rem;
+  width: 80%;
+  border-radius: 8px;
+display: flex;
+justify-content: flex-start;
+align-items: center;
+cursor: pointer;
+margin-bottom: 1rem;
+  transition: all 0.3s ease-in-out;
+
+  & p {
+        margin-left: 20px;
+
+  }
+  &:hover {
+    background-color: #f25252;
+  }
+`;
--- src/Component/UserList/UserList.js
+++ src/Component/UserList/UserList.js
@@ -1,13 +1,21 @@
-import React from 'react'
-import styled from 'styled-components'
-import User from '../User/User'
+import React from "react";
+import styled from "styled-components";
+import User from "../User/User";
 export default function UserList() {
   return (
-    <UserListCont><User/></UserListCont>
-  )
+    <UserListCont>
+      <User />
+      <User />
+    </UserListCont>
+  );
 }
 
-const UserListCont =styled.div`
-        height: 30%;
-
-`
+const UserListCont = styled.div`
+  
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  width: 80%;
+    height: auto;
+`;
